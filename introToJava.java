b27701a83c41044d7e6139e21878208b6b20cd65
XianPearl <101512958+XianPearl@users.noreply.github.com>
2023-10-17 00:16:42 +0530

    Create introToJava.java

---
+++ introToJava.java
@@ -0,0 +1,35 @@
+//WAP in Java to print "Welcome to java"
+
+import java.util.*;
+public class welcome {
+  public static void main (String args[]) {
+    System.out.println("Welcome to java");
+  }
+}
+
+/*Output
+  Welcome to java*/
+
+//WAP to use 2 integers and print their sum, difference and product
+
+import java.util.*;
+public class calc {
+  public static void main (String args[]) {
+    int a=10, b=20;
+    System.out.println ("Sum="+(a+b));
+    System.out.println ("Difference="+(a-b));
+    System.out.println ("Product="+(a*b));
+  }
+}
+
+/*OUTPUT
+  Sum = 30
+  Difference = -10
+  Product = 200  */
+
+//Accept an integer
+
+
+
+
+
