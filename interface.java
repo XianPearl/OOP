c053b4f421ad409d599ebba96157978e238b6561
XianPearl <101512958+XianPearl@users.noreply.github.com>
2023-10-26 20:03:54 +0530

    Create interface.java

--- interface.java
+++ interface.java
@@ -34,4 +34,62 @@ interface i2 {
   void def();
 }
 class A implements i1, i2 {
-  
+   public void display() {
+      System.out.println("displayed from i1");
+   }
+   public void def() {
+      System.out.println("displayed from i2");
+   }
+   public static void main(String args[]) {
+      new A().display();
+      new A().def();
+   }
+}
+
+/*OUTPUT
+   display from i1
+   display from i2   */
+
+//Create a method interface in JAVA and try to implement that interface in some other class.
+
+class i1 {
+   interface i2 {
+      void def();
+   }
+}
+class B implements i1, i2 {
+   public void def() {
+      System.out.println("nested interface");
+   }
+   public static void main(String args[]) {
+      new B().def();
+   }
+}
+
+/*OUTPUT
+   nested interface   */
+
+//WAP to demonstrate the fact that an interface can be inherited from some other interfaces.  
+
+interface i1 {
+   void disp();
+}
+interface i2 extends i1 {
+   void def();
+}
+class B implements i2 {
+   public void display {
+      System.out.println("inherited from i1");
+   }
+   public void def() {
+      System.out.println("display from i2");
+   }
+   public static void main(String args[]) {
+      new B().disp();
+      new B().def();
+   }
+}
+
+/*OUTPUT
+   inherited from i1
+   display from i2    */
