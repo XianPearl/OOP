a3ef6caefc1542f27db0089d81538a7b6babca2b
XianPearl <101512958+XianPearl@users.noreply.github.com>
2023-10-26 21:02:33 +0530

    Create package.java

---
+++ package.java
@@ -0,0 +1,5 @@
+/* Create 2 packages having the names, admission, and education. The admission package will contain classes student_details and library. The student details
+   should contain student name, department and date of borth, which should be displayed and should be inputed through the keyword. The library class should 
+   contain the book details namely, namely book_name, issue_date, return_date which should be inputed through the keyboard and be displayed accordingly.
+   The education package should contain 2 classes assignment and examination. The assignment class will be having assignment_no, assignment_subject, 
+   mark_on_assignment. The values should be user input and be displayed. The examination 
